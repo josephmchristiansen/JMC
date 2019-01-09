191001d94466973fe7ed1f562e859c69838d738e
josephmchristiansen <37519554+josephmchristiansen@users.noreply.github.com>
2019-01-08 18:12:56 -0700

    version 2.0

--- MATRIX/script.js
+++ MATRIX/script.js
@@ -1,12 +1,23 @@
+// When ready...
+window.addEventListener("load",function() {
+	// Set a timeout...
+	setTimeout(function(){
+		// Hide the address bar!
+		window.scrollTo(0, 1);
+	},0);
+});
 var matrix = [];
-var matrix_value_range = 4;
-var matrix_level = 4;
+var matrix_value_range = 6;
+var matrix_level = 6;
 var matrix_memory = [];
 var matrix_memory_values = [];
 var matrix_progress = 0;
-var matrix_mode = "a";
+var matrix_mode = "b";
 var matrix_peek = 1;
 var matrix_settings_status = 1;
+var matrix_prep_time = 3;
+// var matrix_duration_timer;
+
 function matrix_mode_change(mode){
     matrix_mode = mode;
     if(matrix_mode === "a"){
@@ -28,34 +39,69 @@ function matrix_mode_change(mode){
     }
 }
 function matrix_level_change(change){
-    matrix_level += change;
-    document.getElementById("matrix_level_display").innerHTML = matrix_level;
+    var x = document.getElementById('matrix_level_slider').value;
+    matrix_level = x;
+    // if
+    matrix_level = matrix_level +++ change;
+    document.getElementById('matrix_level_slider').value = matrix_level;
+    document.getElementById("matrix_level_display").innerHTML = matrix_level + "&nbsp; TILES";
     document.getElementById("matrix_score_display").innerHTML = matrix_progress + "/" + matrix_level;
     if(matrix_value_range < matrix_level){
         matrix_value_range = matrix_level;
-        document.getElementById("matrix_range_display").innerHTML = matrix_value_range;
+        document.getElementById("matrix_range_display").innerHTML = "NUMBERS:&nbsp;&nbsp;1&nbsp;&nbsp;-&nbsp;&nbsp;" + matrix_value_range;
     }
-    if(matrix_level < 1){
+    if(matrix_level <= 1){
         matrix_level = 1;
-        document.getElementById("matrix_level_display").innerHTML = matrix_level;
+        document.getElementById("matrix_level_display").innerHTML = matrix_level + "&nbsp; TILE";
         document.getElementById("matrix_score_display").innerHTML = matrix_progress + "/" + matrix_level;
+    }else if(matrix_level >= 40){
+        matrix_level = 40;
+        document.getElementById("matrix_level_display").innerHTML = matrix_level + "&nbsp; TILES";
+        document.getElementById("matrix_score_display").innerHTML = matrix_progress + "/" + matrix_level;
+        document.getElementById('matrix_level_slider').value = matrix_level;
     }
 }
 function matrix_value_range_change(change){
-    matrix_value_range += change;
-    document.getElementById("matrix_range_display").innerHTML = matrix_value_range;
+    var x = document.getElementById('matrix_range_slider').value;
+    matrix_value_range = x;
+    // if
+    matrix_value_range = matrix_value_range +++ change;
+    document.getElementById('matrix_range_slider').value = matrix_value_range;
+    document.getElementById("matrix_range_display").innerHTML = "NUMBERS:&nbsp;&nbsp;1&nbsp;&nbsp;-&nbsp;&nbsp;" + matrix_value_range;
     if(matrix_value_range < matrix_level){
         matrix_value_range = matrix_level;
-        document.getElementById("matrix_range_display").innerHTML = matrix_value_range;
+        document.getElementById("matrix_range_display").innerHTML = "NUMBERS:&nbsp;&nbsp;1&nbsp;&nbsp;-&nbsp;&nbsp;" + matrix_value_range;
+    }else if(matrix_value_range >= 1000){
+        matrix_value_range = 1000;
+        document.getElementById("matrix_range_display").innerHTML = "NUMBERS:&nbsp;&nbsp;1&nbsp;&nbsp;-&nbsp;&nbsp;" + matrix_value_range;
+        document.getElementById('matrix_range_slider').value = matrix_value_range;
     }
 }
 
+
+function matrix_prep_time_change(change){
+    var x = document.getElementById('matrix_time_slider').value;
+    matrix_prep_time = x;
+    // if
+    matrix_prep_time = matrix_prep_time +++ change;
+    document.getElementById('matrix_time_slider').value = matrix_prep_time;
+    document.getElementById("matrix_time_display").innerHTML = "PREP TIME:&nbsp;&nbsp;" + matrix_prep_time + "&nbsp;SECONDS";
+    if(matrix_prep_time < 1){
+        matrix_prep_time = 1;
+        document.getElementById("matrix_time_display").innerHTML = "PREP TIME:&nbsp;&nbsp;" + matrix_prep_time + "&nbsp;SECONDS";
+    }else if(matrix_prep_time >= 60){
+        matrix_prep_time = 60;
+        document.getElementById("matrix_time_display").innerHTML = "PREP TIME:&nbsp;&nbsp;" + matrix_prep_time + "&nbsp;SECONDS";
+        document.getElementById('matrix_time_slider').value = matrix_prep_time;
+    }
+}
 
 
 
 function matrix_reset(){
     console.log("\n MATRIX_RESET");
     matrix_mode_change(matrix_mode);
+    document.getElementById("matrix_score_display").innerHTML = matrix_progress + "/" + matrix_level;
     document.getElementById("matrix").style.color = "#fff";
     for(var i = 1; i <= 40; i++){
             var id_temp = "box_" + i;
@@ -142,10 +188,100 @@ function matrix_click(matrix_id){
     }
 }
 
+function matrix_time_start(){
+    setTimeout(matrix_prep_time_end, matrix_prep_time);
+}
+
+var matrix_duration_timer = ['','00','00'];
+var matrix_timer_int;
+var int = 00;
+var sec = 00;
+var min = 0;
+function matrix_timer(action){
+    
+    
+    switch (action) {
+        case 'start':
+            clearInterval(matrix_timer_int);
+            matrix_timer_int = setInterval(matrix_timer_start, 10);
+            function matrix_timer_start(){
+                int++;
+                if(int == 100){
+                    int = 0;
+                    sec += 01;
+                }else if(sec == 60){
+                    sec = 0;
+                    min += 01;
+                    matrix_duration_timer[0] = min;
+                    var mdt0 = matrix_duration_timer[0].toString();
+                    if(mdt0.toString().length < 2){
+                    var min_temp = min.toString();
+                    matrix_duration_timer[0] = "0" + min_temp;
+                }
+                }
+                matrix_duration_timer[1] = sec;
+                matrix_duration_timer[2] = int;
+                
+                var mdt1 = matrix_duration_timer[1].toString();
+                if(mdt1.length < 2){
+                    var sec_temp = sec.toString();
+                    matrix_duration_timer[1] = "0" + sec_temp;
+                }
+                var mdt2 = matrix_duration_timer[2].toString();
+                if(mdt2.length < 2){
+                    var int_temp = int.toString();
+                    matrix_duration_timer[2] = "0" + int_temp;
+                }
+                
+                document.getElementById('matrix_timer').innerHTML = matrix_duration_timer[0] + ":" + matrix_duration_timer[1] + ":" + matrix_duration_timer[2];
+                
+                // console.log("TIMER: " + matrix_duration_timer);
+                
+                if(sec == matrix_prep_time){
+                    matrix_prep_time_end();
+                    console.log("PREP TIME OVER!");
+                }
+            }
+            break;
+        
+        case 'stop':
+            clearInterval(matrix_timer_int);
+            console.log("TIMER: STOP" + matrix_duration_timer);
+            matrix_duration_timer = ['','00','00'];
+            int = 00;
+            sec = 00;
+            min = 00;
+            console.log("TIMER: " + matrix_duration_timer);
+            break;
+            
+        case 'pause':
+            clearInterval(matrix_timer_int);
+            console.log("TIMER PAUSE: " + matrix_duration_timer);
+            break;
+            
+        default:
+            console.log("YOU DONE MESSED UP!");
+    }
+}
+function matrix_prep_time_end(){
+    // document.getElementById(matrix_id).style.background = "hsl(120, 28%, 19%)";
+    for(var i = 1; i <= 40; i++){
+            var id_temp = "box_" + i;
+            document.getElementById(id_temp).style.color = "#6c8bac";
+        }
+}
+
+
+
+
+
 
 var filter = -1;
 function matrix_start() {
-    document.getElementById("matrix_start").innerHTML = "RESTART";
+    matrix_timer('stop');
+    matrix_timer('start');
+    // matrix_time_start();
+    document.getElementById("matrix_start").innerHTML = "PLAY<br>AGAIN";
     matrix_reset();
     console.log("MATRIX_START");
     for(var i = 0; i < matrix_level; i++){
@@ -194,7 +330,7 @@ function matrix_settings_btn(){
         document.getElementById("matrix_settings").style.transform = "translateX(0px)";
         console.log("MATRIX_SETTINGS: 1");
     }else{
-        document.getElementById("matrix_settings").style.transform = "translateX(-283px)";
+        document.getElementById("matrix_settings").style.transform = "translateX(-480px)";
         console.log("MATRIX_SETTINGS: 0");
     }
 }
